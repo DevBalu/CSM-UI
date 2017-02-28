1ea176655483aad73a4000eb2d098b06ad77f246
DevBalu <ghenov.andrei@gmail.com>
2017-02-28 18:06:29 +0200

    add block google add sence & set page_bg trasform in slideshow

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $( document ).ready(function(){
 	$(".button-collapse").sideNav();
 })
+$("#slide_wrapper > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slide_wrapper > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slide_wrapper');
+}, 3000);

ac106fe5a2cd79f7100a7c0e8cd773a34bd59c8b
DevBalu <ghenov.andrei@gmail.com>
2017-03-31 01:22:30 +0300

    create file that contains head elements

---
+++ components/head.php
@@ -0,0 +1,16 @@
+<!-- framework icons -->
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<!-- END framework icons -->
+
+<!-- framework libs -->
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+<!-- END framework libs -->
+
+<!-- fonts -->
+<link type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+<link type="text/css" href="https://fonts.googleapis.com/css?family=Muli|Open+Sans+Condensed:300,700&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+<!-- END fonts -->
+
+<!-- custom style -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+<!-- END custom style -->

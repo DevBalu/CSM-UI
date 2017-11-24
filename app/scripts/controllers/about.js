364870c00e6e8df6f1790f1315a572a9438a467f
DevBalu <ghenov.andrei@gmail.com>
2017-11-24 13:58:58 +0200

    requilification project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deplApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the deplApp
+ */
+angular.module('deplApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deplApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the deplApp
+ */
+angular.module('deplApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

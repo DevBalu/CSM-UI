9ad8709bd2b523975652ca7d5293b1461dca23d5
DevBalu <ghenov.andrei@gmail.com>
2017-12-11 18:09:02 +0200

    /

--- app/assets/js/controllers/search.js
+++ app/assets/js/controllers/search.js
@@ -5,16 +5,13 @@
 angular.module('app')
 
 .controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
-    $scope.liveSearch = function() {
-        console.log("Live search for: " + $scope.search.query);
-	    $http.get('https://steamcommunity.com/').then(function (response) {
-	    	var data = response.data;
-	    	var sesId = data.substring(data.indexOf('g_sessionID = ') + 15, data.indexOf('g_sessionID = ') + 39);
-	    	$http.get(`https://steamcommunity.com/search/SearchCommunityAjax?text=ostap+ben&filter=users&sessionid=${sesId}&steamid_user=false`).then(function(response) {
-	    		console.log(response.data);
-	    	});
-	    });
-    }
+	$scope.liveSearch = function() {
+		console.log("Live search for: " + $scope.search.query);
+
+		$http.get('https://steam-rests.herokuapp.com/?text=' + $scope.search.query).then(function (response) {
+			console.log(response);
+		});
+	}
 
 
 

0f598f71794f61d8c6bda45fffb153e56cedd6c9
DevBalu <ghenov.andrei@gmail.com>
2017-12-12 18:19:50 +0200

    set search controller

--- app/assets/js/controllers/search.js
+++ app/assets/js/controllers/search.js
@@ -5,14 +5,41 @@
 angular.module('app')
 
 .controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
-	$scope.liveSearch = function() {
-		console.log("Live search for: " + $scope.search.query);
+	$scope.pageCount = 1;
+	/*METHODS*/
+	$scope.join = function($arr){
+		var string = $arr.join("   ,   ");
+		return string;
+	}
 
-		$http.get('https://steam-rests.herokuapp.com/?text=' + $scope.search.query).then(function (response) {
-			console.log(response);
+	$scope.pagination = function(){
+		$scope.nrButtons = $scope.search_result_count / 20;
+	}
+	/*END METHODS*/
+
+
+	$scope.liveSearch = function() {
+		$http.get('http://localhost/restHeroku/web/handler.php?text=' + $scope.search.query).then(function (response) {
+			$scope.search_page = response.data.search_page;
+			$scope.search_result_count = response.data.search_result_count;
+			$scope.users = response.data.users;
+
+			if ($scope.search_result_count !== 0) {
+				$scope.pagination();
+			}
 		});
 	}
 
 
+	$scope.nextPage = function (){
+		$scope.pageCount++;
+		console.log($scope.pageCount);
+		$http.get('http://localhost/restHeroku/web/handler.php?text=' + $scope.search.query + '&page=' + $scope.pageCount).then(function(response){
+			console.log(response.data.search_page);
+			$scope.search_page = response.data.search_page;
+			$scope.search_result_count = response.data.search_result_count;
+			$scope.users = response.data.users;
+		});
+	}
 
 }]);

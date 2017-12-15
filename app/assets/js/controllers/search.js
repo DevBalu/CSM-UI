c7949d00abc3c65a3883ba4e61770df7b0acf0f2
DevBalu <ghenov.andrei@gmail.com>
2017-12-15 18:06:21 +0200

    asd

--- app/assets/js/controllers/search.js
+++ app/assets/js/controllers/search.js
@@ -7,9 +7,9 @@ angular.module('app')
 .controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.pageCount = 1;
 	/*METHODS*/
-	$scope.joinn = function($arr){
-		var string = $arr.join("   ,   ");
-		return string;
+	$scope.joinn = function(arr){
+		var string = arr.join("   ,   ");
+		return string
 	}
 
 	$scope.pagination = function(){
@@ -21,16 +21,29 @@ angular.module('app')
 	}
 	/*END METHODS*/
 
-
 	$scope.liveSearch = function() {
+
 		$http.get('http://localhost/CSM-B/web/handler.php?text=' + $scope.search.query).then(function (response) {
 			$scope.search_page = response.data.search_page;
 			$scope.search_result_count = response.data.search_result_count;
 			$scope.users = response.data.users;
 
+			// $scope.rests = JSON.parse(response);
+			console.log(JSON.parse(response.data));
+
+			// var umi = function(arr_users){
+			// 	for( let item in arr_users){
+			// 		var str  = arr_users[item].toString();
+			// 		console.log( unescape(escape( str )) );
+			// 	}
+			// }
+			// $scope.user_match_info = umi( $scope.users );
+
+			// pagination
 			if ($scope.search_result_count !== 0) {
 				$scope.pagination();
 			}
+			// END pagination
 		});
 	}
 

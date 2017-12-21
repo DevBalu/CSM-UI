12b0a760a61724122f77892091b7130f12a4ff75
DevBalu <ghenov.andrei@gmail.com>
2017-12-21 17:59:07 +0200

    sdfs

--- app/assets/js/controllers/search.js
+++ app/assets/js/controllers/search.js
@@ -7,18 +7,22 @@ angular.module('app')
 .controller('SearchCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 	$scope.pageCount = 1;
 	/*METHODS*/
-	$scope.joinn = function(arr){
-		var string = arr.join("   ,   ");
-		return string
-	}
-
 	$scope.pagination = function(){
-		$scope.nrButtons = $scope.search_result_count / 20;
+		var toRes = $scope.search_page * 20;
+		var fromRes = toRes - 19;
+		var result = 'Showing ' + fromRes + ' - ' + toRes + ' of ' + $scope.search_result_count;
+		return result;
 	}
 
 	$scope.decodeChar = function (item) {
 		return unescape( item.toString() );
 	}
+
+	$scope.findChar = function (char, string) {
+		return string.indexOf(char);
+	}
+
+
 	/*END METHODS*/
 
 	$scope.liveSearch = function() {
@@ -26,6 +30,7 @@ angular.module('app')
 		$http.get('http://localhost/CSM-B/web/handler.php?text=' + $scope.search.query).then(function (response) {
 			$scope.search_page = response.data.search_page;
 			$scope.search_result_count = response.data.search_result_count;
+			$scope.success = response.data.success;
 			$scope.users = response.data.users;
 
 			// pagination
@@ -42,7 +47,13 @@ angular.module('app')
 		$http.get('http://localhost/CSM-B/web/handler.php?text=' + $scope.search.query + '&page=' + $scope.pageCount).then(function(response){
 			$scope.search_page = response.data.search_page;
 			$scope.search_result_count = response.data.search_result_count;
+			$scope.success = response.data.success;
 			$scope.users = response.data.users;
+			// pagination
+			if ($scope.search_result_count !== 0) {
+				$scope.pagination();
+			}
+			// END pagination
 		});
 	}
 
